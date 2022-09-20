73e494b82e0a423989d9e4b7f2d587e4b69023b5
jamaludeen <justjamal17@gmail.com>
2022-09-20 16:05:43 +0530

    js-form

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const form=document.getElementById("form");
 const username=document.getElementById("username");
 const email=document.getElementById("email");
-const Password=document.getElementById("password");
+const Password1=document.getElementById("password1");
 const Password2=document.getElementById("password2");
   
     form.addEventListener('submit',e =>{
 	  e.preventDefault();
 	  checkinput();
-  })
+  });
     function checkinput(){
 		const usernamevalue=username.value.trim();
 		const emailvalue=email.value.trim();
-		const passwordvalue=password.value.trim();
+		const password1value=password1.value.trim();
 		const Password2value=Password2.value.trim();
 		
 		if(usernamevalue ===''){
@@ -28,22 +28,30 @@ const Password2=document.getElementById("password2");
 		}else{
 			setSucess(email);
 		}
-		if(passwordvalue === ""){
-			setError(password,'password cannot be blank');
+		if(password1value === ""){
+			setError(password1,'password cannot be blank');
 		}else{
-			setSucess(passsword);
+			setSucess(password1);
 		}
-		if(passwordvalue === ""){
-			setError(password,'password cannot be blank');
-		}else if(passwordvalue !== paswordvalue2){
-			setError(password2,'passsword2 doesnt match')
-		
+		if(password2value === ""){
+			setError(password2,'password cannot be blank');
+		}else if(password1value !== password2value){
+			setError(password2,'passsword doesnt match');
 	    }else{
-			setSucess(passsword);
+			setSucess(password2);
 		}
 	}
+	
 	function isEmail(email){
 		return  /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/.test(email);
 	}
-
-
+    function setError(input,message){
+		const formcontrol=input.parentElement;
+        const small=formcontrol.querySelector('small');
+		formcontrol.className='formcontrol Error';
+		small.innerText=message;
+	}
+	 function setSucess(input,message){
+		const formcontrol=input.parentElement;
+		formcontrol.className='formcontrol Sucess';
+	}
